a9fb0a2f3a2a11f26964d80172cbf7addbc76daf
Denis Perevalov <perevalovds@gmail.com>
2016-10-19 23:42:30 +0500

    fix frame rate

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,76 +2,77 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ufo.load("ufo.png");
-	ufo.setAnchorPercent(0.5, 0.5);
+    ofSetFrameRate(60);
+    ufo.load("ufo.png");
+    ufo.setAnchorPercent(0.5, 0.5);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-	if (ufoX < mouseX) ufoX += 10;
-	if (ufoX > mouseX) ufoX -= 10;
-	if (ufoY < mouseY) ufoY += 10;
-	if (ufoY > mouseY) ufoY -= 10;
+    if (ufoX < mouseX) ufoX += 10;
+    if (ufoX > mouseX) ufoX -= 10;
+    if (ufoY < mouseY) ufoY += 10;
+    if (ufoY > mouseY) ufoY -= 10;
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	ofBackground(128, 192, 255);
-	ofSetColor(255, 255, 255);
-	ufo.draw(ufoX, ufoY, 128, 128);
+    ofBackground(128, 192, 255);
+    ofSetColor(255, 255, 255);
+    ufo.draw(ufoX, ufoY);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
